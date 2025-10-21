e2474e0b8d351caf67411db9f63d67675eff05f1
your name <ankudo1234@gmail.com>
2025-10-22 01:20:15 +0700

    test cua thai an

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -17,22 +17,4 @@ module.exports = router;
 
 
 
-// const express = require("express");
-// const isAuthenticated = require("../utils/isAuthenticated");
-// // Bỏ dòng require ProductController đi vì chúng ta không tạo mới ở đây
-
-// // SỬA LỖI: Chuyển toàn bộ file thành một hàm export ra ngoài.
-// // Hàm này sẽ nhận vào một 'productController' từ bên ngoài (từ app.js).
-// module.exports = function(productController) {
-//   const router = express.Router();
-
-//   // Bây giờ, tất cả các route sẽ sử dụng CÙNG MỘT productController
-//   // mà app.js đã tạo ra.
-//   router.post("/", isAuthenticated, productController.createProduct);
-//   router.get("/", isAuthenticated, productController.getProducts);
-//   router.post("/buy", isAuthenticated, productController.createOrder);
-//   router.get("/buy/:orderId", isAuthenticated, productController.getOrderStatus);
-
-//   return router;
-// };
 
