7add8e70d699d216e7b33ac8af0bd66e67976389
your name <ankudo1234@gmail.com>
2025-10-21 09:27:39 +0700

    hoanthanh - done

--- order/src/config.js
+++ order/src/config.js
@@ -1,9 +1,24 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-module.exports = {
-    mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
-    rabbitMQURI: 'amqp://localhost',
-    rabbitMQQueue: 'orders',
-    port: 3002
-};
+// module.exports = {
+//     mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
+//     rabbitMQURI: 'amqp://localhost',
+//     rabbitMQQueue: 'orders',
+//     port: 3002
+// };
   
+
+  require('dotenv').config();
+
+  module.exports = {
+    // Đọc URI từ biến môi trường, nếu không có thì dùng localhost (cho CI)
+    mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost:27017/ThaianOrderService',
+    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://localhost',
+    
+    // Các queue mà service này tương tác
+    orderQueue: 'orders',
+    productQueue: 'products',
+
+    port: process.env.PORT || 3002
+  };
+
