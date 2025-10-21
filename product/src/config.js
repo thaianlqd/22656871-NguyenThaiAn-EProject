df3e879b362150711eb7ef539e78a609f750f12e
your name <ankudo1234@gmail.com>
2025-10-21 08:53:55 +0700

    hoanthanh - done

--- product/src/config.js
+++ product/src/config.js
@@ -11,12 +11,24 @@
 
 //test: 
 
-require("dotenv").config();
+// require("dotenv").config();
+
+// module.exports = {
+//   port: process.env.PORT || 3001,
+//   mongoURI: process.env.MONGODB_PRODUCT_URI || "mongodb://localhost:27017/ThaianProductService",  
+//   rabbitMQURI: process.env.RABBITMQ_URI || "amqp://guest:guest@localhost:5672",  
+//   exchangeName: "products",
+//   queueName: "products_queue",
+// };
+
+
+//test tiep:
+require('dotenv').config();
 
 module.exports = {
-  port: process.env.PORT || 3001,
-  mongoURI: process.env.MONGODB_PRODUCT_URI || "mongodb://localhost:27017/ThaianProductService",  
-  rabbitMQURI: process.env.RABBITMQ_URI || "amqp://guest:guest@localhost:5672",  
-  exchangeName: "products",
-  queueName: "products_queue",
+    mongoURI: process.env.MONGODB_ORDER_URI || 'mongodb://localhost/orders',
+    // SỬA LỖI: Đọc RabbitMQ URI từ biến môi trường
+    rabbitMQURI: process.env.RABBITMQ_URI || 'amqp://localhost',
+    rabbitMQQueue: 'orders',
+    port: 3002
 };
