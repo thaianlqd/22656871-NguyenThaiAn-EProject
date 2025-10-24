eafba616239c5d0eb38cba12d35b7d645a9b2699
your name <ankudo1234@gmail.com>
2025-10-24 11:51:48 +0700

    sap hoan thanh

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -16,48 +16,30 @@
 // module.exports = router;
 
 
-//=> thai an - update:
-// const express = require("express");
-// const ProductController = require("../controllers/productController");
-// const isAuthenticated = require("../utils/isAuthenticated");
-
-// const router = express.Router();
-// // Tạo một instance duy nhất của Controller
-// const productController = new ProductController();
-
-// // Tất cả các route đều dùng chung instance này
-// router.post("/", isAuthenticated, productController.createProduct);
-// router.get("/", isAuthenticated, productController.getProducts);
-// router.get("/:id", isAuthenticated, productController.getProductById); 
-
-// // Các route liên quan đến việc mua hàng
-// router.post("/buy", isAuthenticated, productController.createOrder);
-// router.get("/buy/:orderId", isAuthenticated, productController.getOrderStatus);
-
-// // THAY ĐỔI QUAN TRỌNG: Export cả router và controller instance
-// module.exports = { router, productController };
-
-
-
-
-
-
-
+=> thai an - update:
 const express = require("express");
+const ProductController = require("../controllers/productController");
 const isAuthenticated = require("../utils/isAuthenticated");
-// Không cần require ProductController nữa
 
-// Phải export ra một HÀM như thế này
-module.exports = function(productController) {
-  const router = express.Router();
+const router = express.Router();
+// Tạo một instance duy nhất của Controller
+const productController = new ProductController();
 
-  // Sử dụng controller được truyền vào
-  router.post("/", isAuthenticated, productController.createProduct);
-  router.get("/", isAuthenticated, productController.getProducts);
-  router.post("/buy", isAuthenticated, productController.createOrder);
-  router.get("/buy/:orderId", isAuthenticated, productController.getOrderStatus);
-  router.get("/:id", isAuthenticated, productController.getProductById); 
+// Tất cả các route đều dùng chung instance này
+router.post("/", isAuthenticated, productController.createProduct);
+router.get("/", isAuthenticated, productController.getProducts);
+router.get("/:id", isAuthenticated, productController.getProductById); 
 
-  return router; // Hàm này trả về router
-};
+// Các route liên quan đến việc mua hàng
+router.post("/buy", isAuthenticated, productController.createOrder);
+router.get("/buy/:orderId", isAuthenticated, productController.getOrderStatus);
+
+// THAY ĐỔI QUAN TRỌNG: Export cả router và controller instance
+module.exports = { router, productController };
+
+
+
+
+
+
 
