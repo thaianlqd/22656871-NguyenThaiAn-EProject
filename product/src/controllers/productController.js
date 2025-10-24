b62b67a72781ef733f0724c5d54e436d81eb7916
your name <ankudo1234@gmail.com>
2025-10-24 12:41:48 +0700

    sap hoan thanh

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -290,7 +290,9 @@ class ProductController {
   async getProductById(req, res) {
     try {
       const product = await Product.findById(req.params.id);
-      if(!product) return res.status(404).json({message: "product not found"});
+      if(!product){
+        return res.status(404).json({message: "product not found"});
+      } 
       res.status(200).json(product);
     } catch (err) {
       return res.status(400).json({ message: "Invalid product ID format" });
