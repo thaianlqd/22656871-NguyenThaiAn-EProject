25440070a38abf42a773a9401dfb4a74a793b842
your name <ankudo1234@gmail.com>
2025-11-01 08:40:36 +0700

    thaian

--- product/src/controllers/productController.js
+++ product/src/controllers/productController.js
@@ -281,8 +281,9 @@ class ProductController {
     const sp = await Product.findById(req.params.id);
 
     if(!sp){
-      res.status(404).json({message : "ko thay sp"});
+      return res.status(404).json({message : "ko tim thay sp"});
     }
+
 
     res.status(200).json(sp);
   }
--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -34,7 +34,7 @@ router.get("/", isAuthenticated, productController.getProducts);
 router.post("/buy", isAuthenticated, productController.createOrder);
 router.get("/buy/:orderId", isAuthenticated, productController.getOrderStatus);
 
-//Phần 8: xem sản phẩm bằng id ở đây nn
+//Phần 8: xem sản phẩm bằng id ở đây nnn
 router.get("/:id", isAuthenticated, productController.getid);
 
 // THAY ĐỔI QUAN TRỌNG: Export cả router và controller instance
