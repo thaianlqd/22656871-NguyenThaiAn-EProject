fc817ab69cf6cf91e439e9e2923b44722dc5b6e9
your name <ankudo1234@gmail.com>
2025-11-01 15:17:29 +0700

    thaian

--- product/src/routes/productRoutes.js
+++ product/src/routes/productRoutes.js
@@ -34,7 +34,7 @@ router.get("/", isAuthenticated, productController.getProducts);
 router.post("/buy", isAuthenticated, productController.createOrder);
 router.get("/buy/:orderId", isAuthenticated, productController.getOrderStatus);
 
-//Phần 8: xem sản phẩm bằng id ở đây nnnnnnnnn
+//Phần 8: xem sản phẩm bằng id ở đây nnnnnnnnnn
 router.get("/:id", isAuthenticated, productController.getid);
 
 // THAY ĐỔI QUAN TRỌNG: Export cả router và controller instance
